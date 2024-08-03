58f6590a628cff25018019f01ba7f4aa6a3ffc64
Harsh Gupta <harshgupta3341@gmail.com>
2024-08-04 01:44:50 +0530

    small addition

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,19 +11,22 @@ const page = async () => {
     <>
       <h2>Employee List</h2>
       <div className="home-page">
-        {employees.map((employee: Employee) => (
-          <Link
-            href={`/employee/${employee._id}`}
-            key={employee._id}
-            className="employee-card"
-          >
-            <p>{employee._id}</p>
-            <h2>{employee.name}</h2>
-          </Link>
-        ))}
+        {employees.length > 0 ? (
+          employees.map((employee: Employee) => (
+            <Link
+              href={`/employee/${employee._id}`}
+              key={employee._id}
+              className="employee-card"
+            >
+              <p>{employee._id}</p>
+              <h2>{employee.name}</h2>
+            </Link>
+          ))
+        ) : (
+          <h3> No Employees in the system</h3>
+        )}
       </div>
     </>
   );
 };
-
 export default page;
